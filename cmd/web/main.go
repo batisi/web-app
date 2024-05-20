7fb37546d725a1b5045fd2f6e819cc3683d5df49
Batasi Batasovich <batisi5872@mail.ru>
2024-05-20 12:19:17 +0500

    upd file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 )
 
@@ -11,6 +12,10 @@ func main() {
 	addr := flag.String("addr", ":4000", "Сетевой адрес HTTP")
 
 	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
@@ -20,9 +25,15 @@ func main() {
 	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	log.Println("Запуск сервера на %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+	infoLog.Printf("Запуск сервера на %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
 
 type neuteredFileSystem struct {
